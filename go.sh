825b0371601ea74adcc45f1a05165b9587dfa7f2
neonblu <>
2020-03-16 14:23:40 -0500

    fixes

--- go.sh
+++ go.sh
@@ -8,29 +8,6 @@ ls
 pwd
 ./run.sh
 # Modified Qortal UI Install script for Docker Image - Credit to Crowetic for initial version
-  echo -e '---DOING APT UPDATE FIRST---'
-  
-  sudo apt update
-  
-  echo -e '--SETTING UP YARN AND NODE APT SOURCES AND INSTALLING DEPENDENCIES FOR NODE ATTEMPT ONE---'
-  
-  sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo -e "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list  
-  curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-
-  echo -e '---INSTALLING NODEJS 12+ SOURCES WITH TWO POSSIBLE OPTIONS IN CASE OF FAILURE OF ONE---'
-  
-  sudo echo "deb https://deb.nodesource.com/node_12.x eoan main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-  sudo echo 'deb-src https://deb.nodesource.com/node_12.x eoan main' >> /etc/apt/sources.list.d/nodesource.list
-  sudo apt update
-  curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-  
-  echo -e '---INSTALLING DEPENDENCIES!---'
-  
-  sudo apt update  
-  sudo apt -y install nodejs yarn npm zip unzip
-
   echo -e '---CLONING ALL UI REPOSITORIES---'
   
   sudo git clone https://github.com/QORT/qortal-ui && sudo git clone https://github.com/QORT/frag-default-plugins && sudo git clone https://github.com/QORT/frag-core && sudo git clone https://github.com/QORT/frag-qortal-crypto
